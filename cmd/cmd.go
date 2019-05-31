a808e610cb8d7d58df2bd8eaae2cc90948c5b4d9
David Said <david.said@neds.com>
2019-05-31 15:00:54 +1000

    Update import path for micro cli

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,10 +4,10 @@ import (
 	"strings"
 
 	ccli "github.com/divisionone/cli"
-	"github.com/divisionone/divisionone/cli"
 	"github.com/divisionone/go-micro/cmd"
 	"github.com/divisionone/micro/api"
 	"github.com/divisionone/micro/bot"
+	"github.com/divisionone/micro/cli"
 	"github.com/divisionone/micro/new"
 	"github.com/divisionone/micro/plugin"
 	"github.com/divisionone/micro/proxy"
